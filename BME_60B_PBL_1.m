c4bfb97620fcfc6ae401592ef05afcaf8c82083f
ipvander <ipvander@uci.edu>
2025-10-22 19:22:12 -0700

    Refactor blackjack game with deck and dealing functions
    Refactored blackjack game script to include deck creation and dealing functions.

--- BME_60B_PBL_1.m
+++ BME_60B_PBL_1.m
@@ -2,15 +2,69 @@
 % 10/21/25
 % This script creates a blackjack game
 
-% Create and shuffle a deck of Cards
-deck = repmat(1:13, 1, 4); % 13 cards, 4 suits, 52 total cards.
-shuffledDeck = deck(randperm(length(deck))); % Shuffle the deck
-
 % Create card names for dialogue
 cardNames = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",...
-    "King", "Queen", "Jack"];
+    "Jack", "Queen", "King"];
 
-% Create cardValues array to store card values (will be later manipulated
-% for aces)
-cardValues = shuffledDeck;
+% Create the deck of cards
+[deckCards, deckSuits, cardValues] = createDeck();
+
+% Ask how many are playing
+numPlayers = input("Enter number of players (including dealer): ");
+
+% Create player and dealer hands
+[playerHands, CardIndex] = dealInitialHands(numPlayers, deckCards, deckSuits, cardValues)
+
+
+
+
+
+
+
+
+
+
+
+
+function [deckCards, deckSuits, cardValues] = createDeck()
+% This function creates a shuffled deck of cards 
+% Create cards and suits
+cards = 1:13;
+suits = ["Spades","Hearts", "Diamonds", "Clubs"];
+
+% Create a grid of 52 cards, numbers 1:13, with assigned suits 1:4
+[cardsGrid, suitsGrid] = ndgrid(cards, suits);
+deckCards = cardsGrid(:); %Creates column of card # repeated 4 times
+deckSuits = suitsGrid(:); %Creates column of suits repeated 13 times each
+
+% Shuffle the deck
+order = randperm(length(deckCards));
+deckCards = deckCards(order);
+deckSuits = deckSuits(order);
+
+% Create cardValues array to store card values. Required so numbers 
+% 11:13 from deckCards can still be indexed in cardNames.
+cardValues = deckCards;
 cardValues(cardValues > 10) = 10;
+end
+
+
+function [playerHands, CardIndex] = dealInitialHands(numPlayers, deckCards, deckSuits, cardValues);
+% Deals two cards to each player (including dealer)
+% Player 1 is the dealer, the rest are human players
+
+    % Initialize a cell array for each player's hand
+    playerHands = cell(1, numPlayers);
+    CardIndex = 1; % Keeps track of where we are in the deck
+
+    % Deal two cards to each player
+    for p = numPlayers:-1:1  % Count down so dealer (player 1) gets last hand
+        % Each player gets 2 cards from the deck
+        playerHands{p}.cards = deckCards(CardIndex:CardIndex+1);
+        playerHands{p}.suits = deckSuits(CardIndex:CardIndex+1);
+        playerHands{p}.values = cardValues(CardIndex:CardIndex+1);
+        
+        % Move deck index forward
+        CardIndex = CardIndex + 2;
+    end
+end
